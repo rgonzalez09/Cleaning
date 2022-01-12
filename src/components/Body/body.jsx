b3ed09b8e8438dfd918655f32702f0f6b07e246c
Rodrigo <rgonzalez09@gmail.com>
2022-01-11 22:43:09 -0500

    body added

--- src/components/Body/body.jsx
+++ src/components/Body/body.jsx
@@ -12,6 +12,46 @@ export default function body() {
       dolore ipsum magna sunt aliquip. Consectetur adipisicing reprehenderit
       duis occaecat. Ipsum pariatur labore sunt veniam et enim occaecat cillum
       irure magna eu nulla est.
+      <br></br>
+      <br></br>
+      Exercitation cillum nostrud sit pariatur nisi proident. Ex consectetur
+      consequat aliqua non eu ad culpa velit reprehenderit amet non. Enim minim
+      deserunt eu laboris laboris veniam ea laborum occaecat magna pariatur eu
+      magna ipsum. Dolore et exercitation sunt esse quis fugiat tempor excepteur
+      tempor. Occaecat voluptate occaecat amet aute nulla laborum pariatur
+      dolore ipsum magna sunt aliquip. Consectetur adipisicing reprehenderit
+      duis occaecat. Ipsum pariatur labore sunt veniam et enim occaecat cillum
+      irure magna eu nulla est.
+      <br></br>
+      <br></br>
+      Exercitation cillum nostrud sit pariatur nisi proident. Ex consectetur
+      consequat aliqua non eu ad culpa velit reprehenderit amet non. Enim minim
+      deserunt eu laboris laboris veniam ea laborum occaecat magna pariatur eu
+      magna ipsum. Dolore et exercitation sunt esse quis fugiat tempor excepteur
+      tempor. Occaecat voluptate occaecat amet aute nulla laborum pariatur
+      dolore ipsum magna sunt aliquip. Consectetur adipisicing reprehenderit
+      duis occaecat. Ipsum pariatur labore sunt veniam et enim occaecat cillum
+      irure magna eu nulla est.
+      <br></br>
+      <br></br>
+      Exercitation cillum nostrud sit pariatur nisi proident. Ex consectetur
+      consequat aliqua non eu ad culpa velit reprehenderit amet non. Enim minim
+      deserunt eu laboris laboris veniam ea laborum occaecat magna pariatur eu
+      magna ipsum. Dolore et exercitation sunt esse quis fugiat tempor excepteur
+      tempor. Occaecat voluptate occaecat amet aute nulla laborum pariatur
+      dolore ipsum magna sunt aliquip. Consectetur adipisicing reprehenderit
+      duis occaecat. Ipsum pariatur labore sunt veniam et enim occaecat cillum
+      irure magna eu nulla est.
+      <br></br>
+      <br></br>
+      Exercitation cillum nostrud sit pariatur nisi proident. Ex consectetur
+      consequat aliqua non eu ad culpa velit reprehenderit amet non. Enim minim
+      deserunt eu laboris laboris veniam ea laborum occaecat magna pariatur eu
+      magna ipsum. Dolore et exercitation sunt esse quis fugiat tempor excepteur
+      tempor. Occaecat voluptate occaecat amet aute nulla laborum pariatur
+      dolore ipsum magna sunt aliquip. Consectetur adipisicing reprehenderit
+      duis occaecat. Ipsum pariatur labore sunt veniam et enim occaecat cillum
+      irure magna eu nulla est.
     </div>
   );
 }
